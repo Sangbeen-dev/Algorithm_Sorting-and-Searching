694d1297ed36a45c907f808914876e31b748bfde
sangbeen-dev <tkdqls960@naver.com>
2021-06-25 19:45:47 +0900

    중복확인

---
+++ src/com/중복확인/Main.java
@@ -0,0 +1,25 @@
+package com.중복확인;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public String solution(int n,int[] arr) {
+		String answer="U";
+		Arrays.sort(arr);
+		for(int i=0; i<arr.length-1; i++) {
+			if(arr[i]==arr[i+1]) return "D";
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Main t = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+		System.out.println(t.solution(n, arr));
+		kb.close();
+	}
+}
